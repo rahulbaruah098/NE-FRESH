a26a23f0124c12eaf14beeefd4597e586f9aa113
Shantanuphukan <sphukan205@gmail.com>
2026-06-08 16:21:54 +0530

    fixed the popup lines when server is closed in terminal, now it doesnt appear, the debugg lines and is clean

--- app.py
+++ app.py
@@ -22,4 +22,9 @@ import routes.api.routes  # noqa: F401
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+        use_reloader=False
+    )
